c97d691de32612ab80ab2e8fa8559214c8d576c5
mhdyaseenvattappara <mhdyaseenvattappara@gmail.com>
2025-12-29 09:11:18 +0530

    build error fixed

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -4,9 +4,9 @@ import { ReactLenis } from '@studio-freight/react-lenis';
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
     return (
-        // @ts-expect-error - React 19 type compatibility issue with Lenis
+
         <ReactLenis root options={{ lerp: 0.08, duration: 1.5, smoothWheel: true }}>
-            {children}
+            {children as any}
         </ReactLenis>
     );
 }
